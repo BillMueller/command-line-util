1e1be02bb37f5d0b2240dd83d296711324c4297b
BuildTools <unconfigured@null.spigotmc.org>
2019-03-20 23:24:44 +0100

    Testing the new Alarm class out (not final).

--- src/main/java/de/billmueller/certutil/Start.java
+++ src/main/java/de/billmueller/certutil/Start.java
@@ -1,10 +1,18 @@
 package de.billmueller.certutil;
 
+import java.io.IOException;
+
 public class Start {
     public static void main(String[] args) {
         Main main = new Main();
-        main.main();
+//        main.main();
 //		main.debug = true;
 //		new Setup().start(main, true);
+        main.printEditor("TEST", false);
+        try {
+            new Thread(new Alarm(new Main(), new Main().stringToIntArray("00:01", null, true), "placeholder message")).start();
+        } catch (IOException ioe) {
+            new Main().printError(ioe.getMessage());
+        }
     }
 }
